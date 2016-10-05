e7e1e29bcc0eed33b7f5bfd6fb84adfab5c71cad
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-10-05 16:12:09 +0300

    added form template of user editor, added fields, but they don't work

--- com_virtuemart/views/cart/tmpl/default_pricelist.php
+++ com_virtuemart/views/cart/tmpl/default_pricelist.php
@@ -347,6 +347,101 @@
     <input type="hidden" name="task" value="setpayment" />
     <input type="hidden" name="controller" value="cart" />
 </form>
+<form method="post" id="adminForm" name="userForm" action="<?php echo JRoute::_('index.php?view=user',$this->useXHTML,$this->useSSL) ?>" class="form-validate">
+
+<?php 
+		echo 'userfields';
+$typefields = array('corefield', 'billto');
+$corefields = VirtueMartModelUserfields::getCoreFields();
+foreach ($typefields as $typefield) {
+    $_k = 0;
+    $_set = false;
+    $_table = false;
+    $_hiddenFields = '';
+
+//             for ($_i = 0, $_n = count($this->userFields['fields']); $_i < $_n; $_i++) {
+    for ($_i = 0, $_n = count($this->userFields['fields']); $_i < $_n; $_i++) {
+	// Do this at the start of the loop, since we're using 'continue' below!
+	if ($_i == 0) {
+	    $_field = current($this->userFields['fields']);
+	} else {
+	    $_field = next($this->userFields['fields']);
+	}
+
+	if ($_field['hidden'] == true) {
+	    $_hiddenFields .= $_field['formcode'] . "\n";
+	    continue;
+	}
+	if ($_field['type'] == 'delimiter') {
+	    if ($_set) {
+		// We're in Fieldset. Close this one and start a new
+		if ($_table) {
+		    echo '	</table>' . "\n";
+		    $_table = false;
+		}
+		echo '</fieldset>' . "\n";
+	    }
+	    $_set = true;
+	    echo '<fieldset>' . "\n";
+	    echo '	<legend>' . "\n";
+	    echo '		' . $_field['title'];
+	    echo '	</legend>' . "\n";
+	    continue;
+	}
+
+
+
+	if (($typefield == 'corefield' && (in_array($_field['name'], $corefields) && $_field['name'] != 'email' && $_field['name'] != 'agreed') )
+		or ($typefield == 'billto' && !(in_array($_field['name'], $corefields) && $_field['name'] != 'email' && $_field['name'] != 'agreed') )) {
+	    if (!$_table) {
+		// A table hasn't been opened as well. We need one here,
+		if ( $typefield == 'corefield') {
+		    echo '<span class="userfields_info">' . $this->corefield_title . '</span>';
+		} else {
+		    echo '<span class="userfields_info">' . $this->vmfield_title . '</span>';
+		}
+		echo '	<table class="adminform user-details">' . "\n";
+		$_table = true;
+	    }
+	    echo '		<tr>' . "\n";
+	    echo '			<td class="key">' . "\n";
+	    echo '				<label class="' . $_field['name'] . '" for="' . $_field['name'] . '_field">' . "\n";
+	    echo '					' . $_field['title'] . ($_field['required'] ? ' *' : '') . "\n";
+	    echo '				</label>' . "\n";
+	    echo '			</td>' . "\n";
+	    echo '			<td>' . "\n";
+	    echo '				' . $_field['formcode'] . "\n";
+	    echo '			</td>' . "\n";
+	    echo '		</tr>' . "\n";
+	}
+    }
+
+    if ($_table) {
+	echo '	</table>' . "\n";
+    }
+    if ($_set) {
+	echo '</fieldset>' . "\n";
+    }
+    $_k = 0;
+    $_set = false;
+    $_table = false;
+    $_hiddenFields = '';
+    reset($this->userFields['fields']);
+}
+
+echo $_hiddenFields;
+?>
+
+
+<?php echo "shipTo fields";
+ echo $this->lists['shipTo']; ?>
+
+<input type="hidden" name="option" value="com_virtuemart" />
+<input type="hidden" name="controller" value="user" />
+<input type="hidden" name="task" value="" />
+<?php echo JHTML::_( 'form.token' ); ?>
+</form>
+
 <script>
 	function addOneToValue(element){
 		let formElement = element.parentElement;
